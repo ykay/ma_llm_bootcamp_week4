5d769d1aa366ad2829e08a38718fc21b3a8a3a06
Tim Lee <tim@codepath.org>
2024-09-22 22:17:43 -0700

    Switch from LangSmith to LangFuse

--- app.py
+++ app.py
@@ -39,6 +39,8 @@ async def generate_response(client, message_history, gen_kwargs):
         if token := part.choices[0].delta.content or "":
             await response_message.stream_token(token)
     
+    await response_message.update()
+
     return response_message
 
 @cl.on_message
@@ -51,7 +53,6 @@ async def on_message(message: cl.Message):
 
     message_history.append({"role": "assistant", "content": response_message.content})
     cl.user_session.set("message_history", message_history)
-    await response_message.update()
 
 if __name__ == "__main__":
     cl.main()
